80104ccd72fcb4fe1fc5625c30b79d5377e88fef
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-07-18 22:20:11 -0500

    added main header section with responsive desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import { Header } from './components/header/Header'
+import { MainHeaders } from './sections/mainHeaders/MainHeaders'
 
 function App() {
 
   return (
     <main className="App">
       <Header/>
+      <MainHeaders/>
     </main>
   )
 }
---
+++ src/sections/mainHeaders/MainHeaders.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MainHeaders.scss";
+
+export const MainHeaders = () => {
+  return (
+    <div className="main-headers-container">
+      <h1 className="main-header-title">Design solutions made easy</h1>
+      <h3 className="main-header-subtitle">
+        With over ten years of experience in various design disciplines, I’m
+        your one-stop shop for your design needs.
+      </h3>
+    </div>
+  );
+};
