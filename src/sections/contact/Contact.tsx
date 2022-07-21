02d4105252db0c0d59e1e8397807af11f7f2e55e
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-07-20 20:11:32 -0500

    added contact section

---
+++ src/sections/contact/Contact.tsx
@@ -0,0 +1,13 @@
+import './Contact.scss';
+
+export const Contact = () => {
+  return (
+    <div className="contact-container">
+        <div className="contact-left">
+            <h1 className="contact-header">Book a call with me</h1>
+            <h3 className="contact-description">I’d love to have a chat to see how I can help you. The best first step is for us to discuss your project during a free consultation. Then we can move forward from there.</h3>
+        </div>
+        <div className="contact-right"></div>
+    </div>
+  )
+}
